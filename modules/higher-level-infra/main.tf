95a77b4d7cb98987cd8821bfc304591bafd7f26f
Devendraappa <devendraappa.kune@gmail.com>
2024-12-20 12:04:08 +0530

    Update main.tf

--- modules/higher-level-infra/main.tf
+++ modules/higher-level-infra/main.tf
@@ -10,7 +10,7 @@ module "ec2" {
   instance_type = var.instance_type
   key_name      = var.key_name
 }
-/*
+
 module "rds" {
   source             = "../rds"
   allocated_storage  = var.rds_allocated_storage
@@ -21,4 +21,4 @@ module "rds" {
   password           = var.rds_password
   db_subnet_group    = var.rds_db_subnet_group
 }
-*/
+
