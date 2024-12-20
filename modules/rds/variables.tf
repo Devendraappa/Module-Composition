483806d6bba36e6771f9fcf9e871ad48977a16e4
Devendraappa <devendraappa.kune@gmail.com>
2024-12-20 11:49:04 +0530

    Update variables.tf

--- modules/rds/variables.tf
+++ modules/rds/variables.tf
@@ -1,43 +1,28 @@
-variable "allocated_storage" {
-  description = "The allocated storage in gigabytes"
-  type        = number
+
+variable "rds_allocated_storage" {
+  type = number
 }
 
-variable "engine" {
-  description = "The database engine to use"
-  type        = string
-  default     = "mysql"
+variable "rds_engine" {
+  type = string
 }
 
-variable "instance_class" {
-  description = "The instance type of the RDS instance"
-  type        = string
-  default     = "db.t3.micro"  # Change to a supported instance type
+variable "rds_instance_class" {
+  type = string
 }
 
-variable "db_name" {
-  description = "The name of the database"
-  type        = string
+variable "rds_db_name" {
+  type = string
 }
 
-variable "username" {
-  description = "The username for the database"
-  type        = string
+variable "rds_username" {
+  type = string
 }
 
-variable "password" {
-  description = "The password for the database"
-  type        = string
-  sensitive   = true
+variable "rds_password" {
+  type = string
 }
 
-variable "parameter_group_name" {
-  description = "The name of the DB parameter group to associate"
-  type        = string
-  default     = "default.mysql8.0"  # Ensure this is a supported parameter group
-}
-
-variable "skip_final_snapshot" {
-  description = "Determines whether a final DB snapshot is created before the DB instance is deleted"
-  type        = bool
+variable "rds_db_subnet_group" {
+  type = string
 }
