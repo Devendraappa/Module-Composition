fbdf8d2065b70d7e701219a2d8d3e308b1c9adb1
Devendraappa <devendraappa.kune@gmail.com>
2024-12-20 09:39:24 +0530

    Create main.tf

---
+++ modules/ec2/main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "web" {
+  count         = var.instance_count
+  ami           = var.ami
+  instance_type = var.instance_type
+  subnet_id     = element(var.subnet_ids, count.index)
+
+  tags = {
+    Name = "WebServerInstance"
+  }
+}
