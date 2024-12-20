3a79b0e162978181d623d605b0ec82c5ef0be210
Devendraappa <devendraappa.kune@gmail.com>
2024-12-20 10:27:14 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,24 +1,29 @@
 variable "vpc_cidr_block" {
   description = "The CIDR block for the VPC"
   type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "public_subnet_count" {
   description = "Number of public subnets"
   type        = number
+  default     = 2
 }
 
 variable "public_subnets" {
   description = "List of public subnet CIDR blocks"
   type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnet_count" {
   description = "Number of private subnets"
   type        = number
+  default     = 2
 }
 
 variable "private_subnets" {
   description = "List of private subnet CIDR blocks"
   type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
 }
