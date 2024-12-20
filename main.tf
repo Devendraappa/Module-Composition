a518f1e574e73d32aebdcd766279c839ea91954e
Devendraappa <devendraappa.kune@gmail.com>
2024-12-20 11:36:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,20 @@
-module "vpc" {
-  source              = "../vpc"
-  cidr_block          = var.vpc_cidr_block
+module "infrastructure" {
+  source              = "./modules/higher-level-infra"
+  vpc_cidr_block      = var.vpc_cidr_block
   public_subnet_count = var.public_subnet_count
   public_subnets      = var.public_subnets
   private_subnet_count = var.private_subnet_count
   private_subnets     = var.private_subnets
+  instance_count      = var.instance_count
+  ami                 = var.ami
+  instance_type       = var.instance_type
+  availability_zone   = var.availability_zone
+  allocated_storage   = var.allocated_storage
+  engine              = var.engine
+  instance_class      = var.instance_class
+  db_name             = var.db_name
+  username            = var.username
+  password            = var.password
+  parameter_group_name = var.parameter_group_name
+  skip_final_snapshot = var.skip_final_snapshot
 }
