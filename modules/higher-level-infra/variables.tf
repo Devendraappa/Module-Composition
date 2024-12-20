88f39078a797a2d08b53022e48c94e932997655a
Devendraappa <devendraappa.kune@gmail.com>
2024-12-20 10:51:46 +0530

    Update variables.tf

--- modules/higher-level-infra/variables.tf
+++ modules/higher-level-infra/variables.tf
@@ -1,20 +1,39 @@
 variable "vpc_cidr_block" {
-description = "The CIDR block for the VPC"
-type = string 
+  description = "The CIDR block for the VPC"
+  type        = string
 }
+
 variable "public_subnet_count" {
-description = "Number of public subnets"
-type = number 
+  description = "Number of public subnets"
+  type        = number
 }
+
 variable "public_subnets" {
-description = "List of public subnet CIDR blocks"
-type = list(string) 
+  description = "List of public subnet CIDR blocks"
+  type        = list(string)
 }
+
 variable "private_subnet_count" {
-description = "Number of private subnets"
-type = number 
+  description = "Number of private subnets"
+  type        = number
 }
+
 variable "private_subnets" {
-description = "List of private subnet CIDR blocks"
-type = list(string) 
+  description = "List of private subnet CIDR blocks"
+  type        = list(string)
+}
+
+variable "instance_count" {
+  description = "Number of EC2 instances"
+  type        = number
+}
+
+variable "ami" {
+  description = "AMI ID for the EC2 instances"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Instance type for the EC2 instances"
+  type        = string
 }
