94eb3090ab15b07b637dad216202888420f47ffc
Devendraappa <devendraappa.kune@gmail.com>
2024-12-20 11:02:01 +0530

    Update variables.tf

--- modules/higher-level-infra/variables.tf
+++ modules/higher-level-infra/variables.tf
@@ -37,3 +37,54 @@ variable "instance_type" {
   description = "Instance type for the EC2 instances"
   type        = string
 }
+
+variable "allocated_storage" {
+  description = "The allocated storage in gigabytes"
+  type        = number
+}
+
+variable "engine" {
+  description = "The database engine to use"
+  type        = string
+}
+
+variable "instance_class" {
+  description = "The instance type of the RDS instance"
+  type        = string
+}
+
+variable "db_name" {
+  description = "The name of the database"
+  type        = string
+}
+
+variable "username" {
+  description = "The username for the database"
+  type        = string
+}
+
+variable "password" {
+  description = "The password for the database"
+  type        = string
+  sensitive   = true
+}
+
+variable "parameter_group_name" {
+  description = "The name of the DB parameter group to associate"
+  type        = string
+}
+
+variable "skip_final_snapshot" {
+  description = "Determines whether a final DB snapshot is created before the DB instance is deleted"
+  type        = bool
+}
+
+variable "vpc_security_group_ids" {
+  description = "List of VPC security groups to associate"
+  type        = list(string)
+}
+
+variable "db_subnet_group_name" {
+  description = "The DB subnet group to associate"
+  type        = string
+}
