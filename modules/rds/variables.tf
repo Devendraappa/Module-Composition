bd510d2f46ce5e451d3134074c794cd9804b8377
Devendraappa <devendraappa.kune@gmail.com>
2024-12-20 09:40:52 +0530

    Create variables.tf

---
+++ modules/rds/variables.tf
@@ -0,0 +1,33 @@
+variable "allocated_storage" {
+description="The allocated storage in gigabytes"
+type=number 
+}
+variable “engine”{
+description="The database engine to use"
+type=string 
+}
+variable “instance_class”{
+description="The instance type of the RDS instance"
+type=string 
+}
+variable “db_name”{
+description="The name of the database"
+type=string 
+}
+variable “username”{
+description="The username for the database"
+type=string 
+}
+variable “password”{
+description="The password for the database"
+type=string 
+sensitive=true 
+}
+variable “parameter_group_name”{
+description="The name of the DB parameter group to associate"
+type=string 
+}
+variable “skip_final_snapshot”{
+description="Determines whether a final DB snapshot is created before the DB instance is deleted"
+type=bool 
+}
