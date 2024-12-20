e804558e467e71056640bb2c311c24e52987a04d
Devendraappa <devendraappa.kune@gmail.com>
2024-12-20 11:08:39 +0530

    Update variables.tf

--- modules/rds/variables.tf
+++ modules/rds/variables.tf
@@ -1,33 +1,40 @@
 variable "allocated_storage" {
-description="The allocated storage in gigabytes"
-type=number 
+  description = "The allocated storage in gigabytes"
+  type        = number
 }
+
 variable "engine" {
-description = "The database engine to use"
-type = string 
+  description = "The database engine to use"
+  type        = string
 }
+
 variable "instance_class" {
-description = "The instance type of the RDS instance"
-type = string 
+  description = "The instance type of the RDS instance"
+  type        = string
 }
+
 variable "db_name" {
-description = "The name of the database"
-type = string 
+  description = "The name of the database"
+  type        = string
 }
+
 variable "username" {
-description = "The username for the database"
-type = string 
+  description = "The username for the database"
+  type        = string
 }
+
 variable "password" {
-description = "The password for the database"
-type = string 
-sensitive = true 
+  description = "The password for the database"
+  type        = string
+  sensitive   = true
 }
+
 variable "parameter_group_name" {
-description = "The name of the DB parameter group to associate"
-type = string 
+  description = "The name of the DB parameter group to associate"
+  type        = string
 }
+
 variable "skip_final_snapshot" {
-description = "Determines whether a final DB snapshot is created before the DB instance is deleted"
-type = bool 
+  description = "Determines whether a final DB snapshot is created before the DB instance is deleted"
+  type        = bool
 }
