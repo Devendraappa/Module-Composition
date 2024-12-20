e16b15b52d6049e6845b9b9d2bb5928218ed7182
Devendraappa <devendraappa.kune@gmail.com>
2024-12-20 11:47:15 +0530

    Update variables.tf

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,24 +1,11 @@
-variable "vpc_id" {
-  description = "The VPC ID"
-  type        = string
-}
-
-variable "subnet_ids" {
-  description = "List of subnet IDs"
-  type        = list(string)
-}
-
-variable "instance_count" {
-  description = "Number of EC2 instances"
-  type        = number
-}
-
-variable "ami" {
-  description = "AMI ID for the EC2 instances"
-  type        = string
+variable "ami_id" {
+  type = string
 }
 
 variable "instance_type" {
-  description = "Instance type for the EC2 instances"
-  type        = string
+  type = string
+}
+
+variable "key_name" {
+  type = string
 }
