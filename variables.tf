14a2961b95c9fab2fa4b1371998f6ca70d97cd8c
Devendraappa <devendraappa.kune@gmail.com>
2024-12-20 10:50:48 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -27,3 +27,21 @@ variable "private_subnets" {
   type        = list(string)
   default     = ["10.0.3.0/24", "10.0.4.0/24"]
 }
+
+variable "instance_count" {
+  description = "Number of EC2 instances"
+  type        = number
+  default     = 2
+}
+
+variable "ami" {
+  description = "AMI ID for the EC2 instances"
+  type        = string
+  default     = "ami-0c55b159cbfafe1f0"  # Example AMI ID
+}
+
+variable "instance_type" {
+  description = "Instance type for the EC2 instances"
+  type        = string
+  default     = "t2.micro"
+}
