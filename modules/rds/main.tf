663c9f949e8600004d01ec08df7a81da0537240b
Devendraappa <devendraappa.kune@gmail.com>
2024-12-20 11:08:21 +0530

    Update main.tf

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,11 +1,10 @@
 resource "aws_db_instance" "default" {
-  allocated_storage    = var.allocated_storage
-  engine               = var.engine
-  instance_class       = var.instance_class
-  name                 = var.db_name
-  username             = var.username
-  password             = var.password
-  parameter_group_name = var.parameter_group_name
-  skip_final_snapshot  = var.skip_final_snapshot
-  
+  allocated_storage     = var.allocated_storage
+  engine                = var.engine
+  instance_class        = var.instance_class
+  db_name               = var.db_name
+  username              = var.username
+  password              = var.password
+  parameter_group_name  = var.parameter_group_name
+  skip_final_snapshot   = var.skip_final_snapshot
 }
