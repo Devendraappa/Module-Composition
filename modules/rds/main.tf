37721be7d68ef6626381c5f75c411d1983203520
Devendraappa <devendraappa.kune@gmail.com>
2024-12-20 11:48:49 +0530

    Update main.tf

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,10 +1,13 @@
-resource "aws_db_instance" "default" {
-  allocated_storage     = var.allocated_storage
-  engine                = var.engine
-  instance_class        = var.instance_class
-  db_name               = var.db_name
-  username              = var.username
-  password              = var.password
-  parameter_group_name  = var.parameter_group_name
-  skip_final_snapshot   = var.skip_final_snapshot
+resource "aws_db_instance" "db" {
+  allocated_storage    = var.allocated_storage
+  engine               = var.engine
+  instance_class       = var.instance_class
+  name                 = var.db_name
+  username             = var.username
+  password             = var.password
+  subnet_group_name    = var.db_subnet_group
+}
+
+output "db_endpoint" {
+  value = aws_db_instance.db.endpoint
 }
