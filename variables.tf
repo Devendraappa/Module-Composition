7ec9236b693cccbddfa6de7a83f0b36487b0e305
Devendraappa <devendraappa.kune@gmail.com>
2024-12-20 11:00:22 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -45,3 +45,64 @@ variable "instance_type" {
   type        = string
   default     = "t2.micro"
 }
+
+variable "allocated_storage" {
+  description = "The allocated storage in gigabytes"
+  type        = number
+  default     = 20
+}
+
+variable "engine" {
+  description = "The database engine to use"
+  type        = string
+  default     = "mysql"
+}
+
+variable "instance_class" {
+  description = "The instance type of the RDS instance"
+  type        = string
+  default     = "db.t2.micro"
+}
+
+variable "db_name" {
+  description = "The name of the database"
+  type        = string
+  default     = "mydb"
+}
+
+variable "username" {
+  description = "The username for the database"
+  type        = string
+  default     = "admin"
+}
+
+variable "password" {
+  description = "The password for the database"
+  type        = string
+  sensitive   = true
+  default     = "password123"
+}
+
+variable "parameter_group_name" {
+  description = "The name of the DB parameter group to associate"
+  type        = string
+  default     = "default.mysql5.7"
+}
+
+variable "skip_final_snapshot" {
+  description = "Determines whether a final DB snapshot is created before the DB instance is deleted"
+  type        = bool
+  default     = true
+}
+
+variable "vpc_security_group_ids" {
+  description = "List of VPC security groups to associate"
+  type        = list(string)
+  default     = []
+}
+
+variable "db_subnet_group_name" {
+  description = "The DB subnet group to associate"
+  type        = string
+  default     = "default"
+}
