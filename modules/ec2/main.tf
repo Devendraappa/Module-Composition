6a142064f5798f25091902b78462ee3c4002c760
Devendraappa <devendraappa.kune@gmail.com>
2024-12-20 11:39:15 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -3,7 +3,6 @@ resource "aws_instance" "web" {
   ami           = var.ami
   instance_type = var.instance_type
   subnet_id     = element(var.subnet_ids, count.index)
-  availability_zone = var.availability_zone  # Specify a supported Availability Zone
 
   tags = {
     Name = "WebServerInstance"
