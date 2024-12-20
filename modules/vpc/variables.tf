143dd901cd007a47f044e4c5697a3a801d887060
Devendraappa <devendraappa.kune@gmail.com>
2024-12-20 09:18:16 +0530

    Create variables.tf

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,24 @@
+variable "cidr_block" {
+  description = "The CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_count" {
+  description = "Number of public subnets"
+  type        = number
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDR blocks"
+  type        = list(string)
+}
+
+variable "private_subnet_count" {
+  description = "Number of private subnets"
+  type        = number
+}
+
+variable "private_subnets" {
+  description = "List of private subnet CIDR blocks"
+  type        = list(string)
+}
