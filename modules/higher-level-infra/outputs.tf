4f5dac97aff1968a762026442bb1b7095b303f20
Devendraappa <devendraappa.kune@gmail.com>
2024-12-20 11:48:04 +0530

    Update outputs.tf

--- modules/higher-level-infra/outputs.tf
+++ modules/higher-level-infra/outputs.tf
@@ -1,9 +1,12 @@
-output vpc_id {
-value = module.vpc.vpc_id 
+output "vpc_id" {
+  value = module.vpc.vpc_id
 }
-output "public_subnet_ids" {
-value = module.vpc.public_subnet_ids 
+
+output "ec2_instance_id" {
+  value = module.ec2.instance_id
 }
-output "private_subnet_ids" {
-value = module.vpc.private_subnet_ids 
+/*
+output "rds_endpoint" {
+  value = module.rds.db_endpoint
 }
+*/
