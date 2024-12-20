2664f78f71dfd6c6aa2e5a8e1b7b6da7ff6de8c4
Devendraappa <devendraappa.kune@gmail.com>
2024-12-20 11:46:48 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,10 +1,9 @@
-resource "aws_instance" "web" {
-  count         = var.instance_count
-  ami           = var.ami
+resource "aws_instance" "app" {
+  ami           = var.ami_id
   instance_type = var.instance_type
-  subnet_id     = element(var.subnet_ids, count.index)
+  key_name      = var.key_name
+}
 
-  tags = {
-    Name = "WebServerInstance"
-  }
+output "instance_id" {
+  value = aws_instance.app.id
 }
