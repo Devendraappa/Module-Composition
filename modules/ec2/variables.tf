4aec82198eb7bb49af9147128544025df55956d2
Devendraappa <devendraappa.kune@gmail.com>
2024-12-20 11:23:39 +0530

    Update variables.tf

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -22,9 +22,3 @@ variable "instance_type" {
   description = "Instance type for the EC2 instances"
   type        = string
 }
-
-variable "availability_zone" {
-  description = "The Availability Zone to launch the instance in"
-  type        = string
-  default     = "ap-south-1a"  # Specify a supported Availability Zone
-}
