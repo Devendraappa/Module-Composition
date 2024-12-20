849e090573062eaa55e69cc843e161c585ad78c4
Devendraappa <devendraappa.kune@gmail.com>
2024-12-20 12:01:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ module "infra" {
   vpc_cidr_block       = "10.0.0.0/16"
   ami_id               = "ami-053b12d3152c0cc71"
   instance_type        = "t2.micro"
-  key_name             = "guru"
+  key_name             = "desktop"
   rds_allocated_storage = 20
   rds_engine           = "mysql"
   rds_instance_class   = "db.t2.micro"
