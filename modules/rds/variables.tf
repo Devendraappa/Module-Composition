1a0d21adcdcb08119b4060165c6783c8a40a44ae
Devendraappa <devendraappa.kune@gmail.com>
2024-12-20 11:18:28 +0530

    Update variables.tf

--- modules/rds/variables.tf
+++ modules/rds/variables.tf
@@ -6,11 +6,13 @@ variable "allocated_storage" {
 variable "engine" {
   description = "The database engine to use"
   type        = string
+  default     = "mysql"
 }
 
 variable "instance_class" {
   description = "The instance type of the RDS instance"
   type        = string
+  default     = "db.t3.micro"  # Change to a supported instance type
 }
 
 variable "db_name" {
@@ -32,6 +34,7 @@ variable "password" {
 variable "parameter_group_name" {
   description = "The name of the DB parameter group to associate"
   type        = string
+  default     = "default.mysql8.0"  # Ensure this is a supported parameter group
 }
 
 variable "skip_final_snapshot" {
