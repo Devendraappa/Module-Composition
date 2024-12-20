db36fb4ec99818cf8be5dd11942566a9a3ed01c0
Devendraappa <devendraappa.kune@gmail.com>
2024-12-20 11:45:30 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,15 +1,8 @@
+
 resource "aws_vpc" "main" {
   cidr_block = var.cidr_block
 }
 
-resource "aws_subnet" "public" {
-  count      = var.public_subnet_count
-  vpc_id     = aws_vpc.main.id
-  cidr_block = element(var.public_subnets, count.index)
-}
-
-resource "aws_subnet" "private" {
-  count      = var.private_subnet_count
-  vpc_id     = aws_vpc.main.id
-  cidr_block = element(var.private_subnets, count.index)
+output "vpc_id" {
+  value = aws_vpc.main.id
 }
