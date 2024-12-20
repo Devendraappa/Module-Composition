d8cdac7dcd4e12206aed659e78743886d295b8f7
Devendraappa <devendraappa.kune@gmail.com>
2024-12-20 09:21:11 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,8 @@
+module "infrastructure" {
+  source              = "./modules/infrastructure"
+  vpc_cidr_block      = var.vpc_cidr_block
+  public_subnet_count = var.public_subnet_count
+  public_subnets      = var.public_subnets
+  private_subnet_count = var.private_subnet_count
+  private_subnets     = var.private_subnets
+}
